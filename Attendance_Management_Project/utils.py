f26406e6578a9a6d136bc9e0589e4e1938350cf0
Amar Pullepu <amarendra2727@gmail.com>
2019-11-15 20:26:35 +0530

    small Changes

--- Attendance_Management_Project/utils.py
+++ Attendance_Management_Project/utils.py
@@ -1,11 +1,25 @@
 import numpy as np
-import cv2
 import face_recognition
 
 def faceRecog(Known_Face_Encoding ,image_np):
     
+    row,col,plane = image_np.shape
+    x, y = 4, 4
 
-    small_frame = cv2.resize(image_np, (0, 0), fx=0.25, fy=0.25)
+    blue_plane = image_np[:,:,0]
+    green_plane = image_np[:,:,1]
+    red_plane = image_np[:,:,2]
+
+    resize_blue_plane = blue_plane[1::x,1::x]
+    resize_green_plane = green_plane[1::x,1::x]
+    resize_red_plane = red_plane[1::x,1::x]
+
+    small_frame = np.zeros((row//x, col//y, plane),np.uint8)
+
+    small_frame[:,:,0] = resize_blue_plane
+    small_frame[:,:,1] = resize_green_plane
+    small_frame[:,:,2] = resize_red_plane
+    
     rgb_small_frame = small_frame[:, :, :3]
 
     # Find all the faces and face encodings in the current frame of video
